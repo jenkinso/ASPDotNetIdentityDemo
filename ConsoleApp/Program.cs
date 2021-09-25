f66c01cb8ad27d70842e43a163e5de2f98dc4ebf
Owen <jenkinsojj@gmail.com>
2021-09-25 17:49:22 +0100

    Added demo for adding a user claim.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
 
 namespace ConsoleApp
 {
@@ -16,10 +17,16 @@ namespace ConsoleApp
             var userStore = new UserStore<IdentityUser>(); // UserStore ==> abstraction of underlying storage layer (database). We'll be using existing EF implementation
             var userManager = new UserManager<IdentityUser>(userStore); // contains CRUD methods for managing identity data (including managing passwords, claims and roles for a user)
 
+            /* // Creating a User demo
             var creationResult = userManager.Create(new IdentityUser("demoUser"), "Password123!");
-
             Console.WriteLine($"Creating a user was successful? {creationResult.Succeeded}. Error count: {creationResult.Errors.Count()}.");
-            
+            */
+
+            // Adding a Claim demo
+            var user = userManager.FindByName("demoUser");
+            var claimResult = userManager.AddClaim(user.Id, new Claim("given_name", "Owen")); // You can add the same claim_type twice??
+            Console.WriteLine($"Claim succeeded? {claimResult.Succeeded}.");
+
             Console.ReadLine();
         }
     }
