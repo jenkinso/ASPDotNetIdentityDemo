4268fb0f02156518f18c6a2654cdfb417039e7e9
Owen <jenkinsojj@gmail.com>
2021-09-25 17:32:24 +0100

    Created first User.
    Added basic example to create a user
    in database. Example shows use of
    UserStore and UserManager classes.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var userStore = new UserStore<IdentityUser>(); // UserStore ==> abstraction of underlying storage layer (database). We'll be using existing EF implementation
+            var userManager = new UserManager<IdentityUser>(userStore); // contains CRUD methods for managing identity data (including managing passwords, claims and roles for a user)
 
+            var creationResult = userManager.Create(new IdentityUser("demoUser"), "Password123!");
+
+            Console.WriteLine($"Creating a user was successful? {creationResult.Succeeded}. Error count: {creationResult.Errors.Count()}.");
+            
+            Console.ReadLine();
         }
     }
 }
